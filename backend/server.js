671497e766b85e2f1b9e7829954872ea6044c378
Esteban  Wagner <wagner.esteban@gmail.com>
2017-04-29 17:11:27 +1000

    Add readme, Add recipe service

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const models = require('./models');
+const recipesService = require('./services/recipes');
 console.log('Starting whats-for-dinner server');
 
 
@@ -6,47 +6,12 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello World!')
 })
 
 app.get('/recipes', async function (req, res) {
-  const recipes = await getRecipes();
+  const recipes = await recipesService.findAll();
   res.send(recipes);
 })
 
-app.listen(3000)
-
-
-async function getRecipes() {
-  const res = await models.Recipe.findAll({
-    include: [
-      {
-        model: models.RecipeItem,
-        as: 'recipeItems',
-        required: false,
-        include: [
-          {
-            model: models.Ingredient,
-            as: 'ingredient',
-            include: [
-              {
-                model: models.Source,
-                as: 'sources',
-              },
-              {
-                model: models.Season,
-                as: 'seasons',
-              },
-            ],
-          },
-        ]
-      },
-    ],
-  });
-  console.log('Recipe', res[0].id, 'RecipeItems Name', res[0].recipeItems[0].name, 'Ingredient ', res[0].recipeItems[0].ingredient.name, 'Source', res[0].recipeItems[0].ingredient.sources[0].name, 'Season', res[0].recipeItems[0].ingredient.seasons[0].name );
-  return res;
-}
-
-const res = models.Ingredient.findAll().then((res) => {
-  console.log('REsults', res[0].id, res[0].name )
-});
+app.listen(3000);
