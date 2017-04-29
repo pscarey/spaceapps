7800636ffe844106b4ceb46008850b37292a0986
pscarey <peter@parallelparking.com.au>
2017-04-29 12:10:25 +1000

    Frontend basic homepage layout

--- frontend/src/home/index.js
+++ frontend/src/home/index.js
@@ -11,6 +11,10 @@
 import React, { PropTypes } from 'react';
 import Page from '../../components/Page';
 
+const ListPoint = props => (
+  <div style={{margin: '0 auto', width: 350, height: 60}}>{props.children}</div>
+);
+
 class HomePage extends React.Component {
   static propTypes = {
     
@@ -24,15 +28,16 @@ class HomePage extends React.Component {
       <Page>
         <div style={{height: 50}}/>
         <h1 style={{width: '100%', textAlign: 'center', fontWeight:400}}>Whats for dinner?</h1>
-        <ul>
-        </ul>
-        <p>
-          <br /><br />
-        </p>
+        <div style={{width: '100%', paddingTop: 40, textAlign: 'center'}}>
+          <ListPoint> Point 1 </ListPoint> <br />
+          <ListPoint> Point 2 </ListPoint> <br />
+          <ListPoint> Point 3 </ListPoint> <br />
+          <ListPoint> Point 4 </ListPoint> <br />
+        </div>
+        
       </Page>
     );
   }
-
 }
 
 export default HomePage;
