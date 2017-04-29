c437caecd1614b1be2cc2155bb474fcc676fd020
Hen-Chow <henchow@gmail.com>
2017-04-29 23:25:28 +1000

    icon on facts update

--- frontend/app/components/HomePage/feature.js
+++ frontend/app/components/HomePage/feature.js
@@ -13,19 +13,19 @@ class Feature extends React.Component {
     return (
       <div style={{fontSize: '1.5em', lineHeight: '30px'}}>
         <Spacer height="40px" />
-        <div style={imgStyle}><img src="assets/icons/Food.svg" width={50} height={50}/></div>
-        <div>Aussies throw out $8 billion of edible food every year</div>
+        <div style={imgStyle}><img src="assets/icons/Pig.svg" width={60} height={60}/></div>
+        <div>Nearly 40% of methane gas emissions comes from farm animals</div>
 
         <Spacer height="40px" />
-        <img src="assets/icons/Coins.svg" width={50} height={50} style={imgStyle}/>
+        <img src="assets/icons/Coins.svg" width={60} height={60} style={imgStyle}/>
         <div>$1,036 of food is thrown away by the average Australian household every year</div>
 
         <Spacer height="40px" />
-        <img src="assets/icons/Food.svg" width={50} height={50}/>
-        <div>Australians discard up to 20% of the food they purchase. This equates to 1 out of every 5 bags of groceries they buy</div>
+        <img src="assets/icons/Deliver.svg" width={60} height={60}/>
+        <div>At least 20% of our <strong>carbon footprint</strong> comes from the food we eat every day</div>
 
         <Spacer height="40px" />
-        <img src="assets/icons/Globe.svg" width={50} height={50}/>
+        <img src="assets/icons/Food.svg" width={60} height={60}/>
         <div>Up to 40% of the average household bin is <strong>food</strong></div>
       </div>
     )
