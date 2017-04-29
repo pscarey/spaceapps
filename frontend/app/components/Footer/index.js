417f5219a4076a1a928735a3e355270b2d870bea
pscarey <peter@parallelparking.com.au>
2017-04-29 20:23:46 +1000

    Colour on App Bar improved, further recipes page improvements

--- frontend/app/components/Footer/index.js
+++ frontend/app/components/Footer/index.js
@@ -3,15 +3,15 @@ import React from 'react';
 function Footer() {
   return (
     <footer style={{
-      height: 100, 
+      height: 150, 
       backgroundColor: '#404040', 
       textAlign: 'center', 
       color: '#ddd', 
-      fontWeight: 100
+      fontWeight: 300
     }}>
       <div style={{height: 5}}/>
-      <p style={{margin: '20px 0 10px 0'}}> Built for NASA Space Apps, Sydney 2017 </p>
-      <p> TODO Team Names </p>
+      <p style={{margin: '40px 0 0 0'}}> Built for NASA Space Apps, Sydney 2017 </p>
+      <p style={{margin: '15px 0 0 0'}}>Peter Carey, Hen Chow, Karina Hudson, Jannene Kyytsonen, Esteban Wagner</p>
     </footer>
   );
 }
